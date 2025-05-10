98efa52308197f0b7ae2b1f8f0fb6b65afa5cc82
DESKTOP-HSDEK0N\RoboCode 10 <robocodekh@gmail.com>
2025-05-10 17:40:49 +0300

    control led with web interface

--- ESP32Server/src/main.cpp
+++ ESP32Server/src/main.cpp
@@ -1,56 +1,81 @@
-#include <Arduino.h>
-#include <Wifi.h>
+#include <WiFi.h>
 #include <WebServer.h>
 
 #define LED_PIN 2
 
-const char* ssid = "Redmi 9C NFC";
-const char* password = "stak_64_64";
+const char *ssid = "Galaxy";
+const char *password = "12344321";
 
-WebServer server (80);
+WebServer server(80);
 
 void hR();
-void switchLED(bool status);
+void switchLED();
 
-bool state = false; 
+bool state = false;
 
-void setup() {
-Serial.begin(115200);
-WiFi.begin(ssid, password);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
 
-Serial.print("Connection to WiFi");
-while (WiFi.status() !=WL_CONNECTED){
-  delay(250);
-  Serial.print(".");
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, false);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", hR);
+  server.on("/led", switchLED);
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "404: Not found");
+  });
+
+  server.begin();
+  Serial.println("Server started!");
 }
-Serial.println("WiFi Connected");
-Serial.println(WiFi.localIP());
 
-server.on("/", hR);
-server.on("/on", switchLED(true));
-server.on("/on", switchLED(false));
-server.begin();
-Serial.println("Start Server!");
+void loop()
+{
+  server.handleClient();
 }
 
-void loop() {
-server.handleClient();
-}
-
-void hR(){
-  String html ="<html>\
-  <head><title> ESP 32 LED</head></title>\
+void hR()
+{
+  String html = "<html>\
+  <head><title>ESP32 LED</title><meta charset='UTF-8'></head>\
   <body>\
-  <p> Светодиод сейчас: "+ String(state ? "ВКЛ" : "ВЫКЛ") + " </p>\
-  <a herf= \"/on\"><button>LED ON</button></a>\
-  <a herf= \"/off\"><button>LED OFF</button></a>\
+  <p>Светодиод сейчас: " +
+                String(state ? "ВКЛ" : "ВЫКЛ") + "</p>\
+  <a href=\"/led?state=on\"><button>LED ON</button></a>\
+  <a href=\"/led?state=off\"><button>LED OFF</button></a>\
   </body>\
   </html>";
 
   server.send(200, "text/html", html);
 }
 
-void switchLED(bool status){
-  digitalWrite(LED_PIN, status);
-  state = status;
+void switchLED()
+{
+  if (server.hasArg("state"))
+  {
+    String value = server.arg("state");
+    if (value == "on")
+    {
+      digitalWrite(LED_PIN, true);
+      state = true;
+    }
+    else if (value == "off")
+    {
+      digitalWrite(LED_PIN, false);
+      state = false;
+    }
+  }
+  server.sendHeader("Location", "/");
+  server.send(303);
 }
